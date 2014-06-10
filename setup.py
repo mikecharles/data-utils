5feea9c255b99721536b8052caa699b31e992f1e
Mike Charles <mike.charles@noaa.gov>
2014-06-10 18:18:40 +0000

    Have a working skeleton

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='model_data_utilities',
+    version='0.1',
+    packages=['gridded_data_utils'],
+    url='https://cpc-devtools.ncep.noaa.gov/trac/projects/gridded_data_utils',
+    license='',
+    author='Mike Charles',
+    author_email='mike.charles@noaa.gov',
+    description='Set of utilities for manipulating gridded data',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+    ]
+)
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import gridded_data_utils.interpolate
+import gridded_data_utils.merge
+import gridded_data_utils.qc
+
+gridded_data_utils.interpolate.test()
+gridded_data_utils.merge.test()
+gridded_data_utils.qc.test()
