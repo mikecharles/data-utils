007a340464881e721c9b93272a03c7e714157c8b
Melissa Ou <melissa.ou@noaa.gov>
2014-07-18 21:03:58 +0000

    Initial version of function to convert values to percentiles was added as well as the file that it resides in (stats.py). Function is called values_to_ptiles. Does not create output files yet. See #1.

---
+++ gridded_data_utils/stats.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+import sys
+import logging
+import bisect
+
+import numpy
+
+"""
+Contains statistical methods.
+"""
+
+# Setup logging (with a custom message format that includes the level name and message, and level INFO)
+logging.basicConfig(format='[%(levelname)s] %(message)s',level=logging.INFO)
+# Change the logging level to DEBUG
+logging.getLogger().setLevel(logging.DEBUG)
+
+def values_to_ptiles(obs_data,climo,ref_ptiles,k):
+    """ Converts values to percentiles based on a set of climatology data containing values with associated percentiles.
+
+    Method assumes that the values being converted follow a Gaussian distribution to estimated the percentiles.
+
+    Parameters
+    ----------
+    obs_data : array_like   
+        1-dimensional (# locations) Numpy array of data to convert
+    climo : array_like
+        2-dimensional (# percentiles x # locations) Numpy array of climatology values associated with a set of percentiles
+    ref_ptiles : array_like
+        1-dimensional (# reference percentiles) Numpy array of reference percentiles that is associated with the percentiles used in the
+        climatology data.
+    k : Constant representing the ratio of the standard deviation of the percentile at the 100th percentile to the last available reference
+        percentile. (std(100th ptile)/std(last ptile))
+
+    Returns
+    -------
+    array_like
+        A 1-dimensional (# locations) Numpy array of percentiles associated with the passed observation data (obs_data).
+
+    Raises
+    ------
+    Exception
+        If input argument data arrays are not the required shape or dimensions or if the percentile calculation returns an exception.
+
+    Examples
+    --------
+
+    >>> ref_ptiles = numpy.array( [0.01,0.02,0.05,0.1,0.15,0.2,0.25,0.33,0.4,0.5,0.6,0.67,0.75,0.8,0.85,0.9,0.95,0.98,0.99] )
+    >>> num_ptiles = ref_ptiles.shape[0]
+    >>>num_locations = 181*360
+    >>> k = 1.343
+    >>> obs_data = numpy.fromfile("/cpc/prod_tst/mcharles/data/observations/land_air/short_range/global/merged_tmean/1deg/05d/2013/08/15/tmean_05d_20130815.bin","float32").astype('float64')
+    >>> climo_data = numpy.fromfile("/cpc/data/climatologies/land_air/short_range/global/merged_tmean_poe/1deg/05d/tmean_clim_poe_05d_0815.bin","float32").astype('float64')
+    >>> climo_data = numpy.reshape(climo_data, (num_ptiles, num_locations))
+
+    >>> obs_ptiles = values_to_ptiles(obs_data,climo_data,ref_ptiles,k)
+    """
+
+    # Initialize returned array var
+    ptiles = None
+
+    logging.debug("--- reference ptiles and size--- : ")
+    logging.debug(str(ref_ptiles))
+    logging.debug(str(ref_ptiles.shape))
+
+    # Print obs and climo data
+    logging.debug("--- obs data and size --- : ")
+    logging.debug(obs_data)
+    logging.debug(str(obs_data.shape))
+
+    
+    #raise Exception("Test except")
+
+    logging.debug("--- climo data and size--- : ")
+    logging.debug(climo)
+    logging.debug(str(climo.shape))
+    logging.debug("ndim = " + str(ref_ptiles.ndim))
+    
+    # Check to see if number of dims are correct
+    # Ref ptiles should be 1, Obs should be 1, Climo 2
+    if ref_ptiles.ndim != 1:
+        raise Exception("Reference percentile array has a dimension of " + str(ref_ptiles.ndim) + ". Must have an array dimension of 1.")
+    if obs_data.ndim != 1:
+        raise Exception("Observation array has a dimension of " + str(obs_data.ndim) + ". Must have an array dimension of 1.")
+    if climo.ndim != 2:
+        raise Exception("Observation array has a dimension of " + str(climo.ndim) + ". Must have an array dimension of 2.")    
+
+    # Check to see if obs, climo, and ref_ptiles correct dimensions
+    # Size of obs should match the size of climo 2nd dim
+    logging.debug("obs " + str(obs_data.shape[0]))
+    logging.debug("climo " + str(climo.shape[1]))
+    if obs_data.shape[0] != climo.shape[1]:
+        raise Exception("Size of obs array does not match size of 2nd dim of climo array.")
+    if climo.shape[0] != ref_ptiles.shape[0]:
+        raise Exception("Size of climo array (1st dim) does not match size reference percentiles array.")
+
+    # Set indices of the last, 50th, and 100th percentile associated with array ref_ptiles
+    ptile_last = numpy.size(ref_ptiles)-1
+    ptile_50th = bisect.bisect(ref_ptiles,0.50)-1
+
+    # Print Estimated 100th ptile
+    #loc = 4
+    #p_100 = k*(climo[ptile_last,loc] - climo[ptile_50th,loc]) + climo[ptile_50th,loc]
+    #logging.debug("est p 100th = " + str(p_100))
+
+    # Print Estimated 0th ptile
+    #p_0 = k*(climo[0,loc] - climo[ptile_50th,loc]) + climo[ptile_50th,loc]
+    #logging.debug("est p 0th = " + str(p_0))
+
+
+    try:    
+        # Calculate the associated or corresponding climatological percentile with the observation array
+        ptiles = numpy.array([    
+            # If obs is nan, set to a nan value
+            numpy.nan
+                if numpy.isnan(obs)
+     
+            # If obs == a climo value, set ptile to the reference climo ptile
+            # NOTE: Python gives a longer ref_ptiles precision value than specified, ie. 0.10000000000000001 instead of 0.1arg
+            else ref_ptiles[numpy.argwhere(climo[:,loc] == obs)[0][0]]
+                if obs in climo[:,loc]
+
+            # If obs >= estimated T100th ptile, set ptile to 1.0
+            else 1.00
+                if obs >= k*(climo[ptile_last,loc] - climo[ptile_50th,loc]) + climo[ptile_50th,loc]
+
+            # If obs > climo[last ptile] AND obs < est T100th, calculate ptile using linear interpolation
+            else ref_ptiles[ptile_last] + ((obs - climo[ptile_last,loc])/(k*(climo[ptile_last,loc] - \
+                climo[ptile_50th,loc]) + climo[ptile_50th,loc] - climo[ptile_last,loc]))*(1-ref_ptiles[ptile_last])
+                if obs > climo[ptile_last,loc] and obs < k*(climo[ptile_last,loc] - climo[ptile_50th,loc]) \
+                + climo[ptile_50th,loc]
+            
+            # If obs <= estimated T0th ptile, set ptile to 0.0
+            else 0.00 
+                if obs <= k*(climo[0,loc] - climo[ptile_50th,loc]) + climo[ptile_50th,loc]
+
+            # If obs < climo 1st AND obs > est T0th, calculate ptile using linear interpolation
+            else ref_ptiles[0] + ((obs - climo[0,loc])/(k*(climo[0,loc] - \
+                climo[ptile_50th,loc]) + climo[ptile_50th,loc] - climo[0,loc])) * (0.00 - ref_ptiles[0])
+                if obs < climo[0,loc] and obs > k*(climo[0,loc] - climo[ptile_50th,loc]) + climo[ptile_50th,loc]
+          
+            # If T1st < Tobs < Tlastptile, calculate ptile using linear interpolation
+            else ref_ptiles[bisect.bisect(climo[:,loc],obs)-1] + \
+            (ref_ptiles[bisect.bisect(climo[:,loc],obs)] - ref_ptiles[bisect.bisect(climo[:,loc],obs)-1]) \
+            * ((obs - climo[bisect.bisect(climo[:,loc],obs)-1,loc])/ \
+            (climo[bisect.bisect(climo[:,loc],obs),loc]-climo[bisect.bisect(climo[:,loc],obs)-1,loc])) 
+                if obs > climo[0,loc] and obs < climo[ptile_last,loc]
+     
+            # Else set to NaN (a climo value may have potentially been a NaN that is necessary)
+            else numpy.nan
+            for loc, obs in enumerate(obs_data)
+        ])
+    except:
+        logging.critical("Percentile calculation failed.")
+        raise Exception("Percentile calculation failed.")
+
+    return ptiles
+
+##### REMOVE WHEN DONE TESTING ####
+
+def main():
+    # Set ref ptiles
+    #ref_ptiles = numpy.array( [0.01,0.02,0.05,0.1,0.15,0.2,0.25,0.33,0.4,0.5,0.6,0.67,0.75,0.8,0.85,0.9,0.95,0.98,0.99] )
+    ref_ptiles = numpy.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
+    
+    num_ptiles = ref_ptiles.shape[0]
+    num_locations = 5 # 5, 181*360
+    k = 1.343
+    
+    '''
+    # Load obs data
+    obs_data = numpy.fromfile("/cpc/prod_tst/mcharles/data/observations/land_air/short_range/global/merged_tmean/1deg/05d/2013/08/15/tmean_05d_20130815.bin","float32").astype('float64')
+
+    # Load climo data
+    climo_data = numpy.fromfile("/cpc/data/climatologies/land_air/short_range/global/merged_tmean_poe/1deg/05d/tmean_clim_poe_05d_0815.bin","float32").astype('float64')
+    climo_data = numpy.reshape(climo_data, (num_ptiles, num_locations))
+    '''
+
+    
+    # Smaller test dataset
+    obs_data = numpy.array([14.9, 21.1, 30.2, 28.4, 12.12])
+    climo_data = numpy.array([
+    [ 15.2,  15.2,  15.2,  15.2,  15.2],
+    [ 16.4,  16.4,  16.4,  16.4,  16.4],
+    [ 17.8,  17.8,  17.8,  17.8,  17.8],
+    [ 18.8,  18.8,  18.8,  18.8,  18.8],
+    [ 20.5,  numpy.nan, 20.5,  20.5,  20.5],
+    [ 22.3,  22.3,  22.3,  22.3,  22.3],
+    [23.7,  23.7,  23.7,  23.7,  23.7],
+    [ 24.6 ,  24.6,  24.6,  24.6,  24.6],
+    [ 27.6,   27.6,   27.6,   27.6,   27.6],
+    ])
+  
+    # Run func
+    # Convert obs values to percentiles
+    obs_ptiles = values_to_ptiles(obs_data,climo_data,ref_ptiles,k)
+    logging.debug("obs ptiles: " + str(obs_ptiles))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
