904c89ae7c87e5eff6c73d70ed0efd74fbeb5925
Mike Charles <mike.charles@noaa.gov>
2014-09-22 20:02:27 +0000

    Renamed all occurrences of 'grid' to 'data' because now a grid is the name of the grid definition, according to data_utils.gridded.grid.Grid().

--- data_utils/gridded/merging.py
+++ data_utils/gridded/merging.py
@@ -5,26 +5,27 @@ Contains methods for merging gridded data.
 import numpy
 
 
-def stack_grids(bottom, top, mask=None):
-    """Stacks one grid on top of another.
+def stack_datasets(bottom, top, mask=None):
+    """Stacks one dataset on top of another.
 
-    If a mask argument is provided, then the final grid will be equal to the
-    top grid wherever mask=1, and the bottom grid everywhere else.
+    If a mask argument is provided, then the final dataset will be equal to the
+    top dataset wherever mask=1, and the bottom dataset everywhere else.
 
-    If a mask argument is not provided, then the final grid will be equal to
-    the top grid wherever the top grid is not missing, and the bottom grid
-    everywhere else.
+    If a mask argument is not provided, then the final dataset will be equal
+    to the top dataset wherever the top dataset is not missing, and the
+    bottom dataset everywhere else.
 
     Parameters
     ----------
     bottom : 2-d array
         2-dimensional (lat x lon) Numpy array of data to serve as the bottom
-        grid
+        dataset
     top : 2-d array
-        2-dimensional (lat x lon) Numpy array of data to serve as the top grid
+        2-dimensional (lat x lon) Numpy array of data to serve as the top
+        dataset
     mask : 2-day array, optional
         2-dimensional (lat x lon) Numpy array, set to 1 wherever you would
-        like the top grid to be selected
+        like the top dataset to be selected
 
     Examples
     --------
@@ -50,13 +51,15 @@ orig_ll_corner, \
 (-90, 0), \
 (90, 359), \
 1)
-    >>> merged_grid = data_utils.gridded.merging.stack_grids(bottom, top)
+    >>> merged_data = data_utils.gridded.merging.stack_datasets(bottom, top)
     """
     if mask:
-        # Wherever the mask is 1, set final_grid to the top grid, bottom elsewhere
+        # Wherever the mask is 1, set final_grid to the top dataset, bottom
+        # elsewhere
         final_grid = numpy.where(mask == 1, top, bottom)
     else:
-        # Wherever the top grid is not NAN, set final_grid to the top grid, bottom elsewhere
+        # Wherever the top dataset is not NAN, set final_grid to the top
+        # dataset, bottom elsewhere
         final_grid = numpy.where(~numpy.isnan(top), top, bottom)
 
     return final_grid
