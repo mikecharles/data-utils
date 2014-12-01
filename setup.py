7550a6f2a29536c35dbae8364a323d778444432f
Mike Charles <mike.charles@noaa.gov>
2014-12-01 10:57:43 -0500

    Added a basic setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='model_post_processing',
+    version='0.1',
+    packages=find_packages(),
+    url='https://cpc-devtools.ncep.noaa.gov/trac/projects/model-post-processing',
+    license='',
+    author='Mike Charles',
+    author_email='mike.charles@noaa.gov',
+    description='Set of utilities for post-processing gridded model data',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+    ],
+    requires=['numpy', 'scipy']
+)
+
+
