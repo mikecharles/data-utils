d43d15e61731e09e301a7df85ff1b5d3d69a66ec
Mike Charles <mike.charles@noaa.gov>
2014-12-02 15:28:20 -0500

    Put all the plotting code into an if statement with an option set to make the plot or not

--- mpp/poe.py
+++ mpp/poe.py
@@ -12,6 +12,7 @@ def make_poe(discrete_members, std):
     # Options
     #
     num_xvals = 1000  # Number of discrete X values for generating PDFs
+    make_plot = False # Whether to make a plot of all steps of the POE creation
 
     #---------------------------------------------------------------------------
     # Create kernels for all members
@@ -38,30 +39,31 @@ def make_poe(discrete_members, std):
     #---------------------------------------------------------------------------
     # Plot all ensemble members
     #
-    matplotlib.rcParams['font.size'] = 10
-    #matplotlib.rcParams['legend.fontsize'] = 'small'
-    # Create a figure
-    fig, ax1 = matplotlib.pyplot.subplots(1, 1)
-    # Loop over all standardized ensemble members and plot their kernel
-    for member in range(num_members):
-        if member == 0:
-            ax1.plot(x, kernels[member], 'b', label='Ensemble member')
-        else:
-            ax1.plot(x, kernels[member], 'b')
-    # Plot the average of all kernels (final PDF)
-    ax1.plot(x, final_pdf, 'r', label='Ensemble PDF')
-    # Plot the average of all kernels in POE form
-    ax2 = ax1.twinx()
-    ax2.plot(x, final_poe, 'k', label='Ensemble POE')
-    # Create legends
-    leg1 = ax1.legend(loc="upper left")
-    ax2.add_artist(leg1)
-    leg2 = ax2.legend(loc="upper right")
-    # Save the plot
-    matplotlib.pyplot.savefig('output.png')
-    # Print the probability of exceeding a z-value of 0
-    print('Probability of exceeding {} Z-values is {}'.format(0, final_poe[
-        stats_utils.stats.find_nearest_index(x, 0)]))
+    if make_plot:
+        matplotlib.rcParams['font.size'] = 10
+        #matplotlib.rcParams['legend.fontsize'] = 'small'
+        # Create a figure
+        fig, ax1 = matplotlib.pyplot.subplots(1, 1)
+        # Loop over all standardized ensemble members and plot their kernel
+        for member in range(num_members):
+            if member == 0:
+                ax1.plot(x, kernels[member], 'b', label='Ensemble member')
+            else:
+                ax1.plot(x, kernels[member], 'b')
+        # Plot the average of all kernels (final PDF)
+        ax1.plot(x, final_pdf, 'r', label='Ensemble PDF')
+        # Plot the average of all kernels in POE form
+        ax2 = ax1.twinx()
+        ax2.plot(x, final_poe, 'k', label='Ensemble POE')
+        # Create legends
+        leg1 = ax1.legend(loc="upper left")
+        ax2.add_artist(leg1)
+        leg2 = ax2.legend(loc="upper right")
+        # Save the plot
+        matplotlib.pyplot.savefig('output.png')
+        # Print the probability of exceeding a z-value of 0
+        print('Probability of exceeding {} Z-values is {}'.format(0, final_poe[
+            stats_utils.stats.find_nearest_index(x, 0)]))
 
 
 if __name__ == '__main__':
