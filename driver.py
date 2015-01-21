9c24c07353aead1b297785dc6f7879d3e3b749f1
Mike Charles <mike.charles@noaa.gov>
2015-01-21 17:10:33 -0500

    Finish coding to process multiple models
    Multiple models is now supported. Multiple models can be supplied on the
    command line as comma-separated models following the `-m, --model`
    options (without spaces!).
    
    First we make sure that each model has matching fhr and grid name, then
    we load all the members of each model into one large NumPy array. After
    that happens, the code has no idea there are different models in the
    array, they are treated as members of the same model.

--- driver.py
+++ driver.py
@@ -20,12 +20,31 @@ from mpp.poe import make_poe, poe_to_terciles
 start_time = time()
 
 
+# Function to see if all list elements are the same
+def all_same(items):
+    return all(x == items[0] for x in items)
+
+
 # Function to validate a date
 def date_arg(value):
     if not re.match("\d{8}|\d{8}-\d{8}|yesterday|today", value):
         raise argparse.ArgumentTypeError("must be in the form YYYYMMDD, "
                                          "YYYYMMDD-YYYYMMDD, yesterday, "
                                          "or today".format(value))
+    else:
+        return value
+
+
+# Function to validate the model
+def model_arg(value):
+    model_list = ['gefsbc', 'cmcebc']
+    regex = ''
+    for model in model_list:
+        regex += '({})|'.format(model)
+    regex = regex.strip('|')
+    if not re.match(regex, value):
+        raise argparse.ArgumentTypeError("must be one of the following: "
+                                         "{}".format(model_list))
     else:
         return value
 
@@ -54,10 +73,11 @@ group.add_argument(
     '-m',
     '--model',
     dest='model',
-    help='model to post-process',
+    help='model to post-process - to combine mult. models (equal weighting), '
+         'separate by commas (no spaces)',
     metavar='<MODEL>',
     required=True,
-    choices=['gefsbc']
+    type=model_arg
 )
 group.add_argument(
     '-c',
@@ -172,10 +192,13 @@ elif args.lead == 'd8-14':
     # lead end
     lead_end = 14
 
+# Get a list of models
+models = args.model.split(',')
+
 # ------------------------------------------------------------------------------
 # Config file options
 #
-# Read options from config file
+# Read some options from config file
 try:
     config = ConfigObj(args.config_file, file_error=True, unrepr=True)
 except Exception as e:
@@ -184,26 +207,14 @@ except Exception as e:
         args.config_file, e))
 try:
     ptiles = config['ptiles']
-    fcst_file_template = config['fcst-data']['file-template']
     climo_file_template = config['climo-data']['file-template']
     out_file_prefix_template = config['output']['file-prefix-template']
-    num_members = config['fcst-data']['gefsbc']['num-members']
-    fhr_int = config['fcst-data']['gefsbc']['fhr-int']
-    R_best = config['fcst-data']['gefsbc']['r-best']
-    grid_name = config['fcst-data']['gefsbc']['grid-name']
 except KeyError as e:
     raise KeyError('One of the config keys is missing or invalid...')
 
 # ------------------------------------------------------------------------------
-# Setup a few more things that depend on config and command-line settings
-#
-# Create a new grid definition
-grid = Grid(grid_name)
-# Get a list of fhrs
-fhrs = range(fhr1, fhr2 + 1, fhr_int)
-# Get list of members
-members = ['{:02d}'.format(m) for m in range(num_members)]
 # Setup logging
+#
 logging.basicConfig(format='%(levelname)s - %(module)s - %(message)s',
                     level=getattr(logging, args.log_level))
 logger = logging.getLogger(__name__)
@@ -223,37 +234,77 @@ for date in generate_date_list(args.start_date, args.end_date):
     #
     logger.info('Loading ensemble forecast data...')
 
+    # Make sure the models have matching grids and fhrs
+    if not all_same(
+            [config['fcst-data'][m]['grid-name'] for m in models]
+    ) or not all_same(
+            [config['fcst-data'][m]['fhr-int'] for m in models]
+    ):
+        logger.fatal('All models must have the same fhr-int and grid-name')
+        sys.exit(1)
+
+    # Get a list of fhrs
+    fhrs = range(fhr1, fhr2 + 1, config['fcst-data'][models[0]]['fhr-int'])
+
+    # Create a new grid definition
+    grid = Grid(config['fcst-data'][models[0]]['grid-name'])
+
+    # Calculate total_num_members
+    total_num_members = 0
+    for model in models:
+        total_num_members += config['fcst-data'][model]['num-members']
     # Initialize data NumPy arrays
-    fcst_data = np.empty((num_members, grid.num_x * grid.num_y))  # all
+    fcst_data = np.empty((total_num_members, grid.num_x * grid.num_y))  # all
     # members/grid points
     temp_data = np.empty((len(fhrs), grid.num_x * grid.num_y))  # all fhrs/grid
     # points
 
-    # Loop over members
-    for m in range(len(members)):
-        member = members[m]
-        logger.debug('Loading member {}'.format(member))
-        # Loop over fhrs
-        for f in range(len(fhrs)):
-            fhr = fhrs[f]
-            # Establish fcst file name
-            fcst_file = replace_vars_in_string(fcst_file_template,
-                                               model=args.model, yyyy=yyyy,
-                                               mm=mm, dd=dd,
-                                               cycle_num=cycle_num,
-                                               cycle=args.cycle, fhr=fhr,
-                                               member=member)
-            # Load data from fcst file
-            logger.debug('Loading data from {}...'.format(fcst_file))
-            temp_data[f] = read_grib(fcst_file, 'grib2', 'TMP',
-                                     '2 m above ground')
-        # Create average or accumulation over fhrs
-        if args.var == 'tmean':
-            fcst_data[m] = np.nanmean(temp_data, axis=0)
-        elif args.var == 'precip':
-            fcst_data[m] = np.nansum(temp_data, axis=0)
+    # --------------------------------------------------------------------------
+    # Loop over models
+    #
+    for model in models:
+        # Read model-related options from config file
+        try:
+            R_best = config['fcst-data'][model]['r-best']
+            grid_name = config['fcst-data'][model]['grid-name']
+            num_members = config['fcst-data'][model]['num-members']
+            data_type = config['fcst-data'][model]['data-type']
+            fcst_file_template = config['fcst-data'][model]['file-template']
+            var_name = config['fcst-data'][model]['vars'][args.var]['name']
+            var_level = config['fcst-data'][model]['vars'][args.var]['level']
+        except KeyError as e:
+            raise KeyError('One of the config keys is missing or invalid...')
 
-        # fcst_data[m].astype('float32').tofile('test_m{}.bin'.format(member))
+        # Get list of members
+        members = ['{:02d}'.format(m) for m in range(num_members)]
+
+        # Loop over members
+        for m in range(len(members)):
+            member = members[m]
+            logger.debug('Loading member {}'.format(member))
+            # Loop over fhrs
+            for f in range(len(fhrs)):
+                fhr = fhrs[f]
+                # Establish fcst file name
+                model_file_str = '-'.join(sorted(args.model.split(',')))
+                fcst_file = replace_vars_in_string(fcst_file_template,
+                                                   model=model, yyyy=yyyy,
+                                                   mm=mm, dd=dd,
+                                                   cycle_num=cycle_num,
+                                                   cycle=args.cycle, fhr=fhr,
+                                                   member=member)
+                # Load data from fcst file
+                logger.debug('Loading data from {}...'.format(fcst_file))
+                temp_data[f] = read_grib(fcst_file, data_type, var_name,
+                                         var_level)
+
+            # Create average or accumulation over fhrs
+            if args.var == 'tmean':
+                fcst_data[m] = np.nanmean(temp_data, axis=0)
+            elif args.var == 'precip':
+                fcst_data[m] = np.nansum(temp_data, axis=0)
+
+            # fcst_data[m].astype('float32').tofile('test_m{}.bin'.format(member))
 
     # Convert fcst data from Kelvin to Celsius
     if args.var == 'tmean':
@@ -322,11 +373,15 @@ for date in generate_date_list(args.start_date, args.end_date):
                                                  model=args.model, var=args.var,
                                                  date=date, cycle=args.cycle,
                                                  lead=args.lead)
+        # Remove any commas in the file name (from multi.models, etc.)
+        out_file_prefix = out_file_prefix.replace(',', '-')
+
+        # Plot terciles to a file
         plot_tercile_probs_to_file(below, near, above, grid,
                                    out_file_prefix+'.png', levels=levels,
                                    colors='tmean_colors')
 
+# Print time taken to finish
 end_time = time()
-
 logger.info('Process took {:.0f} seconds to complete'.format(end_time -
                                                              start_time))
