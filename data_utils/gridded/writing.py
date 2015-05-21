9f05c35b1a8872bb9cb6c7d822748433de100881
Mike Charles <mike.charles@noaa.gov>
2015-05-21 14:35:37 -0400

    Fix bug where below is written out for all columns
    The reshaping section added in a recent commit reshapes below, near, and
    above all to below instead of the respective categories.

--- data_utils/gridded/writing.py
+++ data_utils/gridded/writing.py
@@ -13,8 +13,8 @@ def terciles_to_txt(below, near, above, grid, output_file):
     # Reshape data from one to two dimensions
     if below.ndim == near.ndim == above.ndim == 1:
         below = np.reshape(below, (grid.num_y, grid.num_x))
-        near = np.reshape(below, (grid.num_y, grid.num_x))
-        above = np.reshape(below, (grid.num_y, grid.num_x))
+        near = np.reshape(near, (grid.num_y, grid.num_x))
+        above = np.reshape(above, (grid.num_y, grid.num_x))
     # Open output file
     f = open(output_file, 'w')
     # Loop over all grid points
