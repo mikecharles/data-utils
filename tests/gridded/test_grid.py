6642a9f509b86afc309fb842b13d911494037002
Mike Charles <mike.charles@noaa.gov>
2015-08-24 16:15:34 -0400

    Add test for Grid objects

--- tests/gridded/test_grid.py
+++ tests/gridded/test_grid.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import raises
 from data_utils.gridded import grid
 
 
@@ -15,6 +15,17 @@ def test_create_Grid_object():
     """Ensure the Grid object can be created"""
     for supported_grid in supported_grids:
         grid.Grid(supported_grid)
+
+
+def test_exception_raised_for_unsupported_grid():
+    """Ensure an exception is raised for an unsupported units"""
+    with raises(ValueError):
+        grid.Grid('unsupported')
+
+
+def test_create_custom_Grid():
+    """Ensure a custom Grid object can be created"""
+    grid.Grid(ll_corner=(0, 0), ur_corner=(90, 90), res=1)
 
 
 def test_print_info_returns_something(capfd):
