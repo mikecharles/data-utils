fd38605ce4606420bf738b5ffb724ac55df8604d
Mike Charles <mike.charles@noaa.gov>
2015-08-26 15:59:28 -0400

    Add option to setup.py to run test suite

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup, find_packages, Command
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -29,7 +50,7 @@ setup(
         'basemap>=1.0.7',
         'pyyaml>=3.11',
         'stats-utils>=1.2'
-    ]
+    ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 )
-
-
