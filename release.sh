e6bdfc3b466beb4930c8668665035964af36dc8b
Mike Charles <mike.e.charles@gmail.com>
2015-10-23 13:44:30 -0400

    Use standard ANSI numbers for better compatibility

--- release.sh
+++ release.sh
@@ -33,10 +33,10 @@ else
 fi
 
 # Define some colors for echos
-RED='\033[38;5;160m'
+RED='\033[38;5;1m'
 GREEN='\033[38;5;2m'
 YELLOW='\033[38;5;3m'
-BLUE='\033[38;5;39m'
+BLUE='\033[38;5;4m'
 MAGENTA='\033[38;5;5m'
 CYAN='\033[38;5;6m'
 WHITE='\033[38;5;7m'
