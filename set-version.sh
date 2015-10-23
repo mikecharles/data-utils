52b85b996f30e37fd546529d971bec2385074745
Mike Charles <mike.e.charles@gmail.com>
2015-10-22 21:29:49 -0400

    Only allow executing script from it's own directory
    This is safer because the script needs to do some git stuff, and should be within the git repo when doing so.

--- set-version.sh
+++ set-version.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-# Get path of this script
+# Get name and path of this script
+script_name="$(basename "$0")"
 script_path="$(cd "$(dirname "$0")" && pwd)"
+
+# Only run from within script directory
+if [[ "$PWD" != "$script_path" ]]; then
+  echo "Please execute $script_name from inside the directory it's contained in"
+  exit 1
+fi
 
 # Set the path/name of the VERSION file
 version_file="$script_path/VERSION"
@@ -21,3 +28,5 @@ echo "Updating package to version $new_version"
 
 # Update version in VERSION file
 echo $new_version > $version_file
+
+# Make a git commit with the updated VERSION file
