4a1a5b12e4e383bd06b074c5f5b89152596d031b
Mike Charles <mike.charles@noaa.gov>
2015-10-26 16:46:26 -0400

    Switch to shorter version of ANSI colors

--- release.sh
+++ release.sh
@@ -33,15 +33,15 @@ else
 fi
 
 # Define some colors for printfs
-RED='\033[38;5;1m'
-GREEN='\033[38;5;2m'
-YELLOW='\033[38;5;3m'
-BLUE='\033[38;5;4m'
-MAGENTA='\033[38;5;5m'
-CYAN='\033[38;5;6m'
-WHITE='\033[38;5;7m'
-BLACK='\033[38;5;8m'
-NOCOLOR='\033[m'
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+MAGENTA='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+BLACK='\e[38m'
+NOCOLOR='\e[m'
 
 # Get name and path of this script
 script_name="$(basename "$0")"
