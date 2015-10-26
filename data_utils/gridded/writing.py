13780365ca5aeb3d79ed2ef727a6ef0e3aa2a26d
Mike Charles <mike.charles@noaa.gov>
2015-10-26 14:49:38 -0400

    Add missing data value parameter
    If this parameter is set, then when this value is encountered, it won't
    get formatted as a float when written to a text file (eg. -999.000),
    it'll just be left as is.

--- data_utils/gridded/writing.py
+++ data_utils/gridded/writing.py
@@ -8,7 +8,7 @@ from data_utils.gridded.grid import Grid
 from data_utils.gridded.interpolation import interpolate
 
 
-def terciles_to_txt(below, near, above, grid, output_file):
+def terciles_to_txt(below, near, above, grid, output_file, missing_val=None):
     # Reshape data from one to two dimensions
     if below.ndim == near.ndim == above.ndim == 1:
         below = np.reshape(below, (grid.num_y, grid.num_x))
@@ -21,11 +21,28 @@ def terciles_to_txt(below, near, above, grid, output_file):
     for x in range(grid.num_x):
         for y in range(grid.num_y):
             # TODO: Make the num X and Y sizes dynamic - ex. XXYY vs XXXYYY
-            f.write('{:02.0f}{:02.0f}   {:4.3f}   {:4.3f}   {:4.3f}\n'.format(
-                x+1, y+1,
-                below[y, x],
-                near[y, x],
-                above[y, x])
-            )
+            # If below, near, and above are equal to the missing value
+            # specified, do not format them (leave them as is)
+            if missing_val is not None and \
+                    all([x == missing_val for x in[below[y, x],
+                                                   near[y, x],
+                                                   above[y, x]]]):
+                f.write(
+                    '{:02.0f}{:02.0f}    {:4s}    {:4s}    {:4s}\n'.format(
+                        x + 1,
+                        y + 1,
+                        str(missing_val), str(missing_val), str(missing_val)
+                    )
+                )
+            # If below, near, and above are not equal to the missing value
+            # specified, proceed with formatting them as floats
+            else:
+                f.write(
+                    '{:02.0f}{:02.0f}   {:4.3f}   {:4.3f}   {:4.3f}\n'.format(
+                        x+1, y+1,
+                        below[y, x],
+                        near[y, x],
+                        above[y, x])
+                )
     # Close output file
     f.close()
--- data_utils/station/writing.py
+++ data_utils/station/writing.py
@@ -5,18 +5,23 @@ Contains methods for writing station data to a file.
 """
 
 
-def terciles_to_txt(below, near, above, stn_ids, output_file):
+def terciles_to_txt(below, near, above, stn_ids, output_file, missing_val=None):
     # Open output file
     f = open(output_file, 'w')
     # Loop over all stations
     f.write('id      below    near   above\n'.format())
     for i in range(len(stn_ids)):
-        f.write('{:5s}   {:4.3f}   {:4.3f}   {:4.3f}\n'.format(
-            stn_ids[i],
-            below[i],
-            near[i],
-            above[i]
+        # If below, near, and above are equal to the missing value
+        # specified, do not format them (leave them as is)
+        if missing_val is not None and all([x == missing_val for x in
+                                            [below[i], near[i],
+                                             above[i]]]):
+            f.write('{:5s}   {:4s}     {:4s}    {:4s}\n'.format(
+                stn_ids[i], str(missing_val), str(missing_val), str(missing_val)
+                )
             )
-        )
+        else:
+            f.write('{:5s}   {:4.3f}   {:4.3f}   {:4.3f}\n'.format(stn_ids[i],
+                below[i], near[i], above[i]))
     # Close output file
     f.close()
